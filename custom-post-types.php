7293745de8eae166c716556b1bd95b7e67d04a77
woodwardtw <twwoodward@me.com>
2020-12-16 09:23:52 -0500

    add cpt for profiles
    html for example plan at https://codepen.io/twwoodward/pen/BaLRjgm

--- custom-post-types.php
+++ custom-post-types.php
@@ -201,3 +201,69 @@ function create_narrative_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_narrative_cpt', 0 );
+
+
+//profile custom post type
+
+// Register Custom Post Type profile
+// Post Type Key: profile
+
+function create_profile_cpt() {
+
+  $labels = array(
+    'name' => __( 'Profiles', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Profile', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Profile', 'textdomain' ),
+    'name_admin_bar' => __( 'Profile', 'textdomain' ),
+    'archives' => __( 'Profile Archives', 'textdomain' ),
+    'attributes' => __( 'Profile Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Profile:', 'textdomain' ),
+    'all_items' => __( 'All Profiles', 'textdomain' ),
+    'add_new_item' => __( 'Add New Profile', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Profile', 'textdomain' ),
+    'edit_item' => __( 'Edit Profile', 'textdomain' ),
+    'update_item' => __( 'Update Profile', 'textdomain' ),
+    'view_item' => __( 'View Profile', 'textdomain' ),
+    'view_items' => __( 'View Profiles', 'textdomain' ),
+    'search_items' => __( 'Search Profiles', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into profile', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this profile', 'textdomain' ),
+    'items_list' => __( 'Profile list', 'textdomain' ),
+    'items_list_navigation' => __( 'Profile list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Profile list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'profile', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 100,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-nametag',
+  );
+  register_post_type( 'profile', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_profile_cpt', 0 );
