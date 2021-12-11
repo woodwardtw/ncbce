45eaee074770cedc35c1cc12fc57b0a64fc36afd
woodwardtw <twwoodward@me.com>
2021-12-11 13:34:40 -0500

    get auto acf json going

--- acf.php
+++ acf.php
@@ -504,3 +504,39 @@ function sort_vocab_alpha( $value, $post_id, $field ) {
 }
 
 add_filter('acf/load_value/name=vocabulary', 'sort_vocab_alpha', 10, 3);
+
+
+defined( 'ABSPATH' ) || exit;
+
+	//save acf json
+		add_filter('acf/settings/save_json', 'ncbce_json_save_point');
+		 
+		function ncbce_json_save_point( $path ) {
+		    
+		    // update path
+		    $path = plugin_dir_path(__FILE__) . '/acf-json'; //replace w get_stylesheet_directory() for theme
+		    
+		    
+		    // return
+		    return $path;
+		    
+		}
+
+
+		// load acf json
+		add_filter('acf/settings/load_json', 'ncbce_json_load_point');
+
+		function ncbce_json_load_point( $paths ) {
+		    
+		    // remove original path (optional)
+		    unset($paths[0]);
+		    
+		    
+		    // append path
+		    $paths[] = plugin_dir_path(__FILE__) . '/acf-json';//replace w get_stylesheet_directory() for theme
+		    
+		    
+		    // return
+		    return $paths;
+		    
+		}
